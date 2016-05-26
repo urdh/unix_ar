2a004c900a4a30bad2891de9783ba44d9b6394a8
Remi Rampin <remirampin@gmail.com>
2016-05-26 17:37:48 -0400

    Add tests for writing archives

--- tests.py
+++ tests.py
@@ -1,21 +1,102 @@
+import os
+import shutil
+import tempfile
+import unittest
+
 import unixar
 
 
-archive = unixar.open('/tmp/test.ar', 'w')
+class TestWrite(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._dir = tempfile.mkdtemp(prefix='unixar_test_')
+        os.chdir(cls._dir)
+        with open('h.txt', 'wb') as fp:
+            fp.write(b'hello,\n')
+        with open('w.txt', 'wb') as fp:
+            fp.write(b'world\n')
 
-archive.add('/tmp/h.txt', arcname='h.txt')
-archive.addfile(unixar.ArInfo('w.txt'), open('/tmp/w.txt', 'rb'))
+    @classmethod
+    def tearDownClass(cls):
+        os.chdir('/')
+        shutil.rmtree(cls._dir)
 
-archive.close()
+    def assertEqualExceptTwos(self, value, expected):
+        if len(value) != len(expected):
+            raise AssertionError("%r != %r (lengths don't match)" % (
+                                 value, expected))
+        two = b'2'[0]  # PY3 workaround
+        for i, (v, e) in enumerate(zip(value, expected)):
+            if e != two and v != e:
+                raise AssertionError("%r != %r (first mismatch at %d)" % (
+                                     value, expected, i))
+
+    def test_empty(self):
+        archive = unixar.open('empty.ar', 'w')
+        archive.close()
+
+        with open('empty.ar', 'rb') as fp:
+            self.assertEqual(fp.read(),
+                             b'!<arch>\n')
+
+    def test_add(self):
+        archive = unixar.open('add.ar', 'w')
+        archive.add('h.txt', arcname='he')
+        archive.add('w.txt')
+        archive.close()
+
+        with open('add.ar', 'rb') as fp:
+            self.assertEqualExceptTwos(
+                fp.read(),
+                b'!<arch>\n'
+                b'he              2222222222  2222  '
+                b'2222  100622  7         `\n'
+                b'hello,\n'
+                b'\n'
+                b'w.txt           2222222222  2222  '
+                b'2222  100622  6         `\n'
+                b'world\n')
+
+    def test_addfile(self):
+        archive = unixar.open('addfile.ar', 'w')
+        with open('w.txt', 'rb') as fp:
+            archive.addfile(unixar.ArInfo('w.txt', size=4), fp)
+        with open('h.txt', 'rb') as fp:
+            archive.addfile('w.txt', fp)
+        archive.close()
+
+        with open('addfile.ar', 'rb') as fp:
+            self.assertEqualExceptTwos(
+                fp.read(),
+                b'!<arch>\n'
+                b'w.txt           2222222222  2222  '
+                b'2222  100622  4         `\n'
+                b'worl'
+                b'w.txt           2222222222  2222  '
+                b'2222  100622  6         `\n'
+                b'hello,')
 
 
-archive = unixar.open('/tmp/test.ar', 'r')
+def main():
+    archive = unixar.open('/tmp/test.ar', 'w')
 
-archive.infolist()
-archive.getinfo('h.txt')
+    archive.add('/tmp/h.txt', arcname='h.txt')
+    archive.addfile(unixar.ArInfo('w.txt'), open('/tmp/w.txt', 'rb'))
 
-archive.extract('h.txt')
-archive.extractfile('w.txt')
-archive.extractall()
+    archive.close()
 
-archive.close()
+
+    archive = unixar.open('/tmp/test.ar', 'r')
+
+    archive.infolist()
+    archive.getinfo('h.txt')
+
+    archive.extract('h.txt')
+    archive.extractfile('w.txt')
+    archive.extractall()
+
+    archive.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
