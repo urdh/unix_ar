72e6abc2220f249f771f3d174d87bd009b974436
Remi Rampin <remirampin@gmail.com>
2016-05-27 16:50:43 -0400

    Add tests for reading an archive

--- tests.py
+++ tests.py
@@ -10,20 +10,26 @@ except ImportError:
     import unittest
 
 
-class TestWrite(unittest.TestCase):
+class _BaseTestInTempDir(object):
     @classmethod
     def setUpClass(cls):
         cls._dir = tempfile.mkdtemp(prefix='unixar_test_')
         os.chdir(cls._dir)
-        with open('h.txt', 'wb') as fp:
-            fp.write(b'hello,\n')
-        with open('w.txt', 'wb') as fp:
-            fp.write(b'world\n')
 
     @classmethod
     def tearDownClass(cls):
         os.chdir('/')
         shutil.rmtree(cls._dir)
+
+
+class TestWrite(unittest.TestCase, _BaseTestInTempDir):
+    @classmethod
+    def setUpClass(cls):
+        _BaseTestInTempDir.setUpClass()
+        with open('h.txt', 'wb') as fp:
+            fp.write(b'hello,\n')
+        with open('w.txt', 'wb') as fp:
+            fp.write(b'world\n')
 
     def assertEqualExceptTwos(self, value, expected):
         if len(value) != len(expected):
@@ -81,25 +87,57 @@ class TestWrite(unittest.TestCase):
                 b'hello,')
 
 
-def main():
-    archive = unixar.open('/tmp/test.ar', 'w')
+class TestRead(_BaseTestInTempDir, unittest.TestCase):
+    def test_extract(self):
+        with open('archive.ar', 'wb') as fp:
+            fp.write(
+                b'!<arch>\n'
+                b'h.txt           1464380987  501   '
+                b'20    100644  7         `\n'
+                b'hello,\n'
+                b'\n'
+                b'w.txt           1464380990  501   '
+                b'20    100644  6         `\n'
+                b'world\n')
 
-    archive.add('/tmp/h.txt', arcname='h.txt')
-    archive.addfile(unixar.ArInfo('w.txt'), open('/tmp/w.txt', 'rb'))
+        archive = unixar.open('archive.ar', 'r')
+        self.assertEqual(
+            [(e.name, e.size, e.mtime, e.perms, e.uid, e.gid, e.offset)
+             for e in archive.infolist()],
+            [(b'h.txt', 7, 1464380987, 0o100644, 501, 20, 8),
+             (b'w.txt', 6, 1464380990, 0o100644, 501, 20, 8 + 60 + 8)])
 
-    archive.close()
+        info = archive.getinfo('w.txt')
+        self.assertEqual(
+            (info.name, info.size, info.mtime,
+             info.perms, info.uid, info.gid, info.offset),
+            (b'w.txt', 6, 1464380990, 0o100644, 501, 20, 8 + 60 + 8))
 
+        archive.extract(info)
+        with open('w.txt', 'rb') as fp:
+            self.assertEqual(fp.read(), b'world\n')
 
-    archive = unixar.open('/tmp/test.ar', 'r')
+        os.mkdir('sub')
 
-    archive.infolist()
-    archive.getinfo('h.txt')
+        archive.extract('h.txt', 'sub')
+        with open('sub/h.txt', 'rb') as fp:
+            self.assertEqual(fp.read(), b'hello,\n')
 
-    archive.extract('h.txt')
-    archive.extractfile('w.txt')
-    archive.extractall()
+        info.name = 'sub/other.txt'
+        info.size = 3
+        archive.extract(info)
+        with open('sub/other.txt', 'rb') as fp:
+            self.assertEqual(fp.read(), b'wor')
 
-    archive.close()
+        os.mkdir('all')
+
+        archive.extractall('all')
+        with open('all/h.txt', 'rb') as fp:
+            self.assertEqual(fp.read(), b'hello,\n')
+        with open('all/w.txt', 'rb') as fp:
+            self.assertEqual(fp.read(), b'world\n')
+
+        archive.close()
 
 
 if __name__ == '__main__':
